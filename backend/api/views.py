3b019d6396508d9ad87162c0ee45c61e0821c700
Oybek Narzullayev <oybeknarzullaev99@gmail.com>
2022-04-06 11:22:17 +0500

    rest framework response & view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,17 @@
 from django.forms import model_to_dict
 from django.http import JsonResponse
-import json
 from products.models import Product
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API view
+    """
+    if request.method == 'POST':
+        return Response(data={"message": "POST not allowed"}, status=405)
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
@@ -14,6 +22,6 @@ def api_home(request, *args, **kwargs):
         # yuqoridagi commentga teng kuchli quyida:
         data = model_to_dict(model_data, fields=['id']) # only get id
         data = model_to_dict(model_data) # default -> all fields
-    return JsonResponse(data)
+    return Response(data=data, status=200)
     
 
--- python_client/basic.py
+++ python_client/basic.py
@@ -4,6 +4,4 @@ endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, params={"abs": 123}, json={"query": "Hello world"})
 
-print("Response status: ",get_response.status_code)
-print("Response text format: ", get_response.text)
-print("Response json format", get_response.json())
+print(f"status: {get_response.status_code}\ndata: {get_response.json()}")
