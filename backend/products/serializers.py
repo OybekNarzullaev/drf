b245f479504f2edc6cca8f3b5b43e76c65e058cf
Oybek Narzullayev <oybeknarzullaev99@gmail.com>
2022-06-04 14:38:18 +0500

    urls reverse serizalizers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializers(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
+            'edit_url',
+            'url',
             'id',
             'title',
             'content',
@@ -12,5 +17,17 @@ class ProductSerializers(serializers.ModelSerializer):
             'sale_price',
             'discount'
         ]
+
+    def get_url(self, obj):
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return reverse('product-detail', kwargs={"pk":obj.pk}, request=request)
+    
+    def get_edit_url(self, obj):
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return reverse('product-edit', kwargs={"pk":obj.pk}, request=request)
 
 
